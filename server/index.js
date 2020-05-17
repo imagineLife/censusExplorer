e0bb71419be9524390698c8b86428887cbc8f4c7
Eric Laursen <ericjlaursen@gmail.com>
2020-05-17 13:46:26 -0400

    reconfigs init

--- server/index.js
+++ server/index.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const {connectToMongo} = require('./connectToDB')
+const { setEnvVars }  = require('./setEnvVars')
 
-try{
-	connectToMongo();
-}catch(e){
-	console.log('MONGO ERROR')
-	console.log('e');
-	
+async function appInit(){
+	try{
+		await setEnvVars();
+		await connectToMongo();
+		console.log('INIT DONE!');
+
+	// setup express 
+		const app = express();
+		app.use(express.static("public"));
+
+
+		app.get('/', (req,res) => res.json({"dummy": "object result"}))
+
+		app.listen(process.env.PORT, () => {
+		  console.log(`Your app is listening on port ${process.env.PORT}`);
+		});
+	}catch(e){
+		console.log('MONGO ERROR')
+		console.log(e);
+	}	
 }
 
-const app = express();
-app.use(express.static("public"));
-
-
-app.get('/', (req,res) => res.json({"dummy": "object result"}))
-
-app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${process.env.PORT}`);
-});
+appInit();
