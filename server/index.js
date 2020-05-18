d612112ae29db390dead0bcf0a59ba400738740d
Eric Laursen <ericjlaursen@gmail.com>
2020-05-17 20:40:10 -0400

    hand-written to-be-recursive object-shape getter

--- server/index.js
+++ server/index.js
@@ -1,17 +1,73 @@
 const express = require("express");
-const {connectToMongo} = require('./connectToDB')
+const {connectToMongo, startMongo} = require('./connectToDB')
 const { setEnvVars }  = require('./setEnvVars')
 
 async function appInit(){
 	try{
 		await setEnvVars();
-		await connectToMongo();
+
+		//connect && retrieve single element
+		// await connectToMongo();
 		console.log('INIT DONE!');
 
 	// setup express 
 		const app = express();
 		app.use(express.static("public"));
 
+
+		app.get('/shape', startMongo, async (req, res) => {
+			console.log('SHAPE!');
+			let firstDataElement = await req.dbCollection.findOne()
+			
+			let resObj = {}
+			let keys = Object.keys(firstDataElement);
+
+			//loop through object keys
+			keys.forEach(k => {
+				if(k !== '_id'){
+
+					//get key-value
+					let keyedDataVal  = firstDataElement[k]
+					let isObj = typeof keyedDataVal === 'object'
+					if(!isObj){
+						resObj[k] = typeof keyedDataVal
+						return;
+					}
+
+					//IF key-value is an object...
+					// loop through object keys
+					resObj[k] = {}
+					let objValKeys = Object.keys(keyedDataVal)
+					objValKeys.forEach(ovk => {
+						let nestedKeyedDataVal  = firstDataElement[k][ovk]
+						
+						let isNestedObj = typeof nestedKeyedDataVal === 'object'
+						if(!isNestedObj){
+							// console.log(`NOT nested obj: ${k}`);
+							resObj[k][ovk] = typeof keyedDataVal
+							return;
+						}
+
+						//IF key-value is an object...
+					// loop through object keys
+						resObj[k][ovk] = {}
+						let nestedObjKeys = Object.keys(nestedKeyedDataVal)
+						nestedObjKeys.forEach(nestedObjKey => {
+							let doubleNestedKeyedDataVal  = firstDataElement[k][ovk][nestedObjKey]
+							let isDoubleNestedObj = typeof doubleNestedKeyedDataVal === 'object'
+							if(!isDoubleNestedObj){
+								// console.log(`NOT nested obj: ${k}`);
+								resObj[k][ovk][nestedObjKey] = typeof doubleNestedKeyedDataVal
+								return;
+							}
+							console.log('IS DOUBLE NESTED OBJ');
+							console.log(nestedObjKey)
+						})
+					})
+				}
+			})
+			return res.json(resObj)
+		})
 
 		app.get('/', (req,res) => res.json({"dummy": "object result"}))
 
