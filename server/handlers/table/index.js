70b6e4b09528bf39e899e4529e6b47b558a6a931
Eric Laursen <ericjlaursen@gmail.com>
2020-05-18 12:40:57 -0400

    returns firstRow

--- server/handlers/table/index.js
+++ server/handlers/table/index.js
@@ -4,52 +4,7 @@ const tableHandler = async (req, res) => {
 	try{
 		let firstDataElement = await req.dbCollection.findOne()
 	
-		let resObj = {}
-		let keys = Object.keys(firstDataElement);
-
-		//loop through object keys
-		keys.forEach(k => {
-			if(k !== '_id'){
-
-				//get key-value
-				let keyedDataVal  = firstDataElement[k]
-				let objType = isObject(keyedDataVal)
-				if(!objType){
-					resObj[k] = typeof keyedDataVal
-					return;
-				}
-
-				//IF key-value is an object...
-				// loop through object keys
-				resObj[k] = {}
-				let objValKeys = Object.keys(keyedDataVal)
-				objValKeys.forEach(ovk => {
-					let nestedKeyedDataVal  = firstDataElement[k][ovk]
-					
-					let isNestedObj = isObject(nestedKeyedDataVal)
-					if(!isNestedObj){
-						// console.log(`NOT nested obj: ${k}`);
-						resObj[k][ovk] = typeof keyedDataVal
-						return;
-					}
-
-					//IF key-value is an object...
-				// loop through object keys
-					resObj[k][ovk] = {}
-					let nestedObjKeys = Object.keys(nestedKeyedDataVal)
-					nestedObjKeys.forEach(nestedObjKey => {
-						let doubleNestedKeyedDataVal  = firstDataElement[k][ovk][nestedObjKey]
-						let isDoubleNestedObj = isObject(doubleNestedKeyedDataVal)
-						if(!isDoubleNestedObj){
-							// console.log(`NOT nested obj: ${k}`);
-							resObj[k][ovk][nestedObjKey] = typeof doubleNestedKeyedDataVal
-							return;
-						}
-					})
-				})
-			}
-		})
-		return res.json(resObj)
+		return res.json(firstDataElement)
 	}catch(e){
 		console.log('e')
 		console.log(e)
