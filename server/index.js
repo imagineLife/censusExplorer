bf53880ee5de9e07828bb28447447652607751c9
Eric Laursen <ericjlaursen@gmail.com>
2020-05-18 07:15:45 -0400

    leverages handlers directory

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {connectToMongo, startMongo} = require('./connectToDB')
 const { setEnvVars }  = require('./setEnvVars')
+const {shapeHandler} = require('./handlers/shape')
 const { 
 	isObj,
 	notArr,
@@ -10,71 +11,17 @@ async function appInit(){
 	try{
 		await setEnvVars();
 
-		//connect && retrieve single element
-		// await connectToMongo();
-		console.log('INIT DONE!');
-
-	// setup express 
+		// setup express 
 		const app = express();
 		app.use(express.static("public"));
 
 
-		app.get('/shape', startMongo, async (req, res) => {
-			console.log('SHAPE!');
-			let firstDataElement = await req.dbCollection.findOne()
-			
-			let resObj = {}
-			let keys = Object.keys(firstDataElement);
+		//setup express route-handling
+		app.get('/shape', startMongo, shapeHandler)
 
-			//loop through object keys
-			keys.forEach(k => {
-				if(k !== '_id'){
-
-					//get key-value
-					let keyedDataVal  = firstDataElement[k]
-					let objType = isObject(keyedDataVal)
-					if(!objType){
-						resObj[k] = typeof keyedDataVal
-						return;
-					}
-
-					//IF key-value is an object...
-					// loop through object keys
-					resObj[k] = {}
-					let objValKeys = Object.keys(keyedDataVal)
-					objValKeys.forEach(ovk => {
-						let nestedKeyedDataVal  = firstDataElement[k][ovk]
-						
-						let isNestedObj = isObject(nestedKeyedDataVal)
-						if(!isNestedObj){
-							// console.log(`NOT nested obj: ${k}`);
-							resObj[k][ovk] = typeof keyedDataVal
-							return;
-						}
-
-						//IF key-value is an object...
-					// loop through object keys
-						resObj[k][ovk] = {}
-						let nestedObjKeys = Object.keys(nestedKeyedDataVal)
-						nestedObjKeys.forEach(nestedObjKey => {
-							let doubleNestedKeyedDataVal  = firstDataElement[k][ovk][nestedObjKey]
-							let isDoubleNestedObj = isObject(doubleNestedKeyedDataVal)
-							if(!isDoubleNestedObj){
-								// console.log(`NOT nested obj: ${k}`);
-								resObj[k][ovk][nestedObjKey] = typeof doubleNestedKeyedDataVal
-								return;
-							}
-						})
-					})
-				}
-			})
-			return res.json(resObj)
-		})
-
-		app.get('/', (req,res) => res.json({"dummy": "object result"}))
 
 		app.listen(process.env.PORT, () => {
-		  console.log(`Your app is listening on port ${process.env.PORT}`);
+		  console.log(`---API listening on port ${process.env.PORT}---`);
 		});
 	}catch(e){
 		console.log('MONGO ERROR')
