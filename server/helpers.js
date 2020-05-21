5f7c1464b0ca77e4388fd45655edd22967f79607
Eric Laursen <ericjlaursen@gmail.com>
2020-05-20 20:11:04 -0400

    flatten row

--- server/helpers.js
+++ server/helpers.js
@@ -2,9 +2,11 @@
 const isObj = itm => typeof itm === "object"
 const notArr = itm => !Array.isArray(itm)
 const isObject = itm => isObj(itm) && notArr(itm)
+const flattenRow = (obj) => obj;
 
 module.exports = {
 	isObj,
 	notArr,
-	isObject
+	isObject,
+	flattenRow
 }
