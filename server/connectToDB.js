6be7a6db8045781bcdf255941507f3b16d4bfe94
Eric Laursen <ericjlaursen@gmail.com>
2020-05-22 19:41:21 -0400

    removes unused function

--- server/connectToDB.js
+++ server/connectToDB.js
@@ -1,32 +1,5 @@
 const {MongoClient} = require('mongodb');
 
-//connect to mongo instance
-async function connectToMongo(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = process.env.mongoURL;
- 
-
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        // Make the appropriate DB calls
-        statesCollection = await client.db("povertystates").collection('states');
-		statesCollection.find().limit(1).forEach(foundItem => console.log(JSON.stringify(foundItem)));
- 
-    } catch (e) {
-        console.error(e);
-        return {"Error": e}
-    } finally {
-        await client.close();
-    }
-}
-
 const startMongo = async (req, res, next) => {
      const uri = process.env.mongoURL;
  
@@ -38,7 +11,7 @@ const startMongo = async (req, res, next) => {
         await client.connect();
  
         // Make the appropriate DB calls
-        statesCollection = await client.db("povertystates").collection('states');
+        let statesCollection = await client.db("povertystates").collection('states');
 
         //pass collection to req. obj
         req.dbCollection = statesCollection;
@@ -48,4 +21,4 @@ const startMongo = async (req, res, next) => {
         return {"Error": e}
     }
 }
-module.exports = { connectToMongo, startMongo }
+module.exports = { startMongo }
