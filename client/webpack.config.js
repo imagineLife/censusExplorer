7647c05d9277e4f0c77b647d7b3e8440864adbf6
Eric Laursen <ericjlaursen@gmail.com>
2020-05-24 17:39:49 -0400

    css handling

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
 		    }
 		  },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.[cs]?[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
           'style-loader',
