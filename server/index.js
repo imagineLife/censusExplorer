a9f6436d114eb37f984e1543a6d1d4e0344cbf80
Eric Laursen <ericjlaursen@gmail.com>
2020-05-23 20:11:00 -0400

    statistic handler(s)

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { startMongo } = require('./connectToDB')
 const { setEnvVars }  = require('./setEnvVars')
-const { shapeHandler } = require('./handlers/shape')
-const { tableHandler } = require('./handlers/table')
+const { 
+	tableHandler, 
+	shapeHandler,
+	statisticHandler
+} = require('./handlers')
 async function appInit(){
 	try{
 		await setEnvVars();
@@ -16,6 +19,8 @@ async function appInit(){
 		app.get('/shape', startMongo, shapeHandler)
 		app.get('/table', startMongo, tableHandler)
 		app.get('/table/:state', startMongo, tableHandler)
+		app.get('/statistic', startMongo, statisticHandler)
+		// app.get('/statistic/:type', startMongo, statisticHandler)
 
 		app.listen(process.env.PORT, () => {
 			console.log(`---API listening on port ${process.env.PORT}---`);
