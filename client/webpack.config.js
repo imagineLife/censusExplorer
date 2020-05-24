42b4ec9e3f8ea1dac581fd2366f4c8771d117cd9
Eric Laursen <ericjlaursen@gmail.com>
2020-05-24 17:24:46 -0400

    sass+css loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -16,7 +16,18 @@ module.exports = {
 		    use: {
 		      loader: 'babel-loader'
 		    }
-		  }
+		  },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
     ]
   },
   plugins: [
