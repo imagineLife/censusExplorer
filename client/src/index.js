da678c45388bd80e0b9eb57404a095a8ee19cefd
Eric Laursen <ericjlaursen@gmail.com>
2020-05-24 14:23:55 -0400

    init

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+// import App from "./App";
+import style from "./main.css";
+
+const App = () => <p></p>
+ReactDOM.render(<App />, document.getElementById("app"));
