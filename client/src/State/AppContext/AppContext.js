07b3cd0c83752078b10641f60ab264972d7f0aa4
Eric Laursen <ericjlaursen@gmail.com>
2020-05-25 08:55:52 -0400

    init

---
+++ client/src/State/AppContext/AppContext.js
@@ -0,0 +1,7 @@
+import React, { createContext } from 'react';
+
+const AppContext = () => {
+	return (<p>ctx</p>)
+};
+
+export default AppContext;
---
+++ client/src/State/AppContext/index.js
@@ -0,0 +1,2 @@
+import AppContext from './AppContext';
+export default AppContext;
