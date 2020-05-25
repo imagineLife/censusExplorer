4357dd067886cd6e085ac1a3cb3ea569e3600bb6
Eric Laursen <ericjlaursen@gmail.com>
2020-05-25 09:36:17 -0400

    null statsData state

--- client/src/State/AppContext/AppContext.js
+++ client/src/State/AppContext/AppContext.js
@@ -1,12 +1,17 @@
-import React, { createContext } from 'react';
+import React, { 
+	createContext,
+	useState
+} from 'react';
 
 const AppContext = createContext();
 const { Provider } = AppContext;
 
 const AppProvider = ({children}) => {
-	// let [statsData, setStatsData] = useState(null)
+	let [statsData, setStatsData] = useState(null)
 	return(
-		<Provider value={{dummy: "value"}}>
+		<Provider value={{
+			statsData
+		}}>
 			{children}
 		</Provider>
 	)
