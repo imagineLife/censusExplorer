376f6f98f73914aaeaa090db9f3665f61db2d9de
Eric Laursen <ericjlaursen@gmail.com>
2020-05-25 13:52:47 -0400

    await instead of then

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,11 +1,9 @@
 const fetcher = async (url, opts) => {
 	let fetchOpts = opts || {}
 	let fetchRes = await fetch(url, fetchOpts)
-	
 	if(fetchRes.status == 200){
-		fetchRes.json().then(jsonRes => {
-			return { result: jsonRes }
-		})
+		let jsonRes = await fetchRes.json();
+			return jsonRes
 	}else{
 		fetchRes.json().then(jsonRes => {
 			return {"Error": "non-200"}	
