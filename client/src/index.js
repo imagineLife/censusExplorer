2128e1b5c7082bcb5846e4de628ccb494f730f55
Eric Laursen <ericjlaursen@gmail.com>
2020-05-25 09:36:27 -0400

    StatsViewer init

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,17 @@
-import React from "react"
+import React, { useContext } from "react"
 import ReactDOM from "react-dom"
 import './normalize.css'
 import "./index.scss";
 import { AppProvider } from './State/AppContext/AppContext'
 
-const App = () => <section id="app-wrapper">App Component</section>;
+//Components
+import StatsViewer from './StatsViewer'
 
 const rootElement = document.getElementById("app")
 
 if(rootElement !== null){
 	ReactDOM.render(
 		<AppProvider>
-			<App />
+			<StatsViewer />
 		</AppProvider>, rootElement);
 }
