798a0ec423e11453a65166fe3056494e2edf8f52
Eric Laursen <ericjlaursen@gmail.com>
2020-05-25 15:25:45 -0400

    statsKeysHandler

--- server/handlers/index.js
+++ server/handlers/index.js
@@ -1,8 +1,10 @@
 const { shapeHandler } = require('./shape')
+const { statsKeysHandler } = require('./statsKeys')
 const { tableHandler } = require('./table')
 const { statisticHandler } = require('./statistic')
 module.exports = {
 	shapeHandler,
 	tableHandler,
-	statisticHandler
+	statisticHandler,
+	statsKeysHandler
 }
