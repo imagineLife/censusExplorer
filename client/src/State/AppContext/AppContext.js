b3c115b8e5ccaaeb83b9c7abae192f6ff6862530
Eric Laursen <ericjlaursen@gmail.com>
2020-05-25 09:45:07 -0400

    fetcher && fetchedStats state

--- client/src/State/AppContext/AppContext.js
+++ client/src/State/AppContext/AppContext.js
@@ -3,11 +3,17 @@ import React, {
 	useState
 } from 'react';
 
+//helper fns
+import { fetcher } from './../../helpers'
 const AppContext = createContext();
 const { Provider } = AppContext;
 
 const AppProvider = ({children}) => {
 	let [statsData, setStatsData] = useState(null)
+	let [fetchedStats, setFetchedStats] = useState(false)
+
+	
+
 	return(
 		<Provider value={{
 			statsData
