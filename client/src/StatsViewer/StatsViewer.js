f08686e93ffd3b6a6e4412294e03fe4ea65a2fe4
Eric Laursen <ericjlaursen@gmail.com>
2020-05-25 14:28:44 -0400

    mock dd content

--- client/src/StatsViewer/StatsViewer.js
+++ client/src/StatsViewer/StatsViewer.js
@@ -2,6 +2,10 @@ import React, {
 	useContext,
 	Fragment 
 } from 'react';
+import './statsViewer.scss'
+
+//Components
+import Dropdown from './../Components/Dropdown'
 
 //State
 import { AppContext } from './../State/AppContext/AppContext'
@@ -14,9 +18,20 @@ const StatsViewer = () => {
 
 	if(statsData){
 		svContent = <Fragment>
-				<header>
+				<header className="stats-header">
 					<h2>Statistics Highlights</h2>
 					<sub>Details on a single statistic</sub>
+					<Dropdown
+						label="Currently viewing:"
+					  className="stat-picker"
+					  onClick={() => { console.log('CLICKED DD')}}
+					>
+						<p>1</p>
+						<p>2</p>
+						<p>3</p>
+						<p>4</p>
+						<p>5</p>
+					</Dropdown>
 				</header>
 			</Fragment>
 
