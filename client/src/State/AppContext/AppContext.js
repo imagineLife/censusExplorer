9a321a6112e4c848cf91076a313aa4b994eb12fc
Eric Laursen <ericjlaursen@gmail.com>
2020-05-26 19:52:25 -0400

    fetches stats keys && removes css file

--- client/src/State/AppContext/AppContext.js
+++ client/src/State/AppContext/AppContext.js
@@ -10,21 +10,39 @@ const AppContext = createContext();
 const { Provider } = AppContext;
 
 const AppProvider = ({children}) => {
-	const [statsData, setStatsData] = useState(null)
-	const [fetchedStats, setFetchedStats] = useState(false)
+	const [selectedStatKey, setSelectedStatKey] = useState('percentBelowPoverty.gender.male');
+	const [statsList, setStatsList] = useState(null)	//list of statistics && selected-state
+	const [statsData, setStatsData] = useState(null)	//single-stat data
+	const [fetchedStatsList, setFetchedStatsList] = useState(false)
+	const [fetchedStat, setFetchedStat] = useState(false)
 	const [statsUrl] = useState(`${process.env.SERVER_HOST}/statistic`);
+	const [statsKeysUrl] = useState(`${process.env.SERVER_HOST}/statsKeys`);
 
-	//fetch stats 
+	//fetch stats keys
 	useEffect(() => {
-		if(!statsData && !fetchedStats){
+		if(!statsList && !fetchedStatsList){
+			const fetchStatsKeys = async () => {
+				let statsRes = await fetcher(statsKeysUrl)
+				setStatsData(statsRes)
+			}
+			fetchStatsKeys();
+			setFetchedStatsList(true)
+		}
+	}, [statsData, fetchedStat])
+
+	//fetch single-stat
+	useEffect(() => {
+		if(!statsData && !fetchedStat){
 			const fetchStats = async () => {
 				let statsRes = await fetcher(statsUrl)
 				setStatsData(statsRes)
 			}
 			fetchStats();
-			setFetchedStats(true)
+			setFetchedStat(true)
 		}
-	}, [statsData, fetchedStats])
+	}, [statsData, fetchedStat])
+
+	
 	return(
 		<Provider value={{
 			statsData
