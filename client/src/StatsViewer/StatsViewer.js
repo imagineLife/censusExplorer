7a8027570f15d0812b1bb87cda7d40969379f8f0
Eric Laursen <ericjlaursen@gmail.com>
2020-05-27 05:16:08 -0400

    extracts props from Component Rendering

--- client/src/StatsViewer/StatsViewer.js
+++ client/src/StatsViewer/StatsViewer.js
@@ -22,17 +22,17 @@ const StatsViewer = () => {
 	let svContent = <p>loading stats data...</p>
 
 	if(statsData && selectedStatKey && statsList){
-
+		const ddProps = {
+			displayText: selectedStatKey,
+			label: "Currently viewing:",
+		  className: "stat-picker",
+		  onClick: () => { console.log('CLICKED DD')}
+		}
 		svContent = <Fragment>
 				<header className="stats-header">
 					<h2>Statistics Highlights</h2>
 					<sub>Details on a single statistic</sub>
-					<Dropdown
-						displayText={selectedStatKey}
-						label="Currently viewing:"
-					  className="stat-picker"
-					  onClick={() => { console.log('CLICKED DD')}}
-					>
+					<Dropdown {...ddProps} >
 						{statsList.map(({string, selected}, idx) => (
 							<p key={`single-stat-${string}`}>{string}</p>)
 						)}
