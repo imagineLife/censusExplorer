02c2086bccea34724c395300f1d86af6a575fb6d
Eric Laursen <ericjlaursen@gmail.com>
2020-05-30 09:25:35 -0400

    init routerLookup arr

--- client/src/Router/Router.js
+++ client/src/Router/Router.js
@@ -5,6 +5,33 @@ import {
   Route
 } from "react-router-dom";
 
-const RouterComponent = () => (<p>Router</p>);
+// Components
+import StatsViewer from './../StatsViewer'
+
+const RouterComponent = () => {
+	const routerLookup = [
+		{
+			path: '/',
+			component: StatsViewer
+		}
+	]
+	return <Router>
+		<Switch>
+			{routerLookup.map((r, idx) => {
+					const ThisRoute = r.component;
+					let props = r.props || {}
+					return (
+						<Route
+							key={`${r.component}-${idx}`}
+							path={r.path}
+						>
+							<ThisRoute {...props} />
+						</Route>
+					)
+				})
+			}
+		</Switch>
+	</Router>
+};
 
 export default RouterComponent;
