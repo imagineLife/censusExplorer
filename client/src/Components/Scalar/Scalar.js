b8deb5109b1fc092221202081ea0f3d791340a1c
Eric Laursen <ericjlaursen@gmail.com>
2020-05-30 13:56:30 -0400

    responsive scalar-val margin-tops

--- client/src/Components/Scalar/Scalar.js
+++ client/src/Components/Scalar/Scalar.js
@@ -3,9 +3,11 @@ import './Scalar.scss'
 
 const Scalar = ({label, value, className}) => {
 	let thisClass = "scalar col-6-2"
+	
 	if(className){
 		thisClass = `${thisClass} ${className}`
 	}
+	
 	return(
 		<figure className={thisClass}>
 			<h3 className="scalar-label">{label}</h3>
