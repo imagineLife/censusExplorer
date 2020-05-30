f86ac3ecd6bf691b64da37071edab30f384bc8ff
Eric Laursen <ericjlaursen@gmail.com>
2020-05-30 09:19:50 -0400

    init router

---
+++ client/src/Router/Router.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+const RouterComponent = () => (<p>Router</p>);
+
+export default RouterComponent;
