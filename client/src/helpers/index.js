1bbb67f8efd359caa9928f94c505c3db1839317c
Eric Laursen <ericjlaursen@gmail.com>
2020-06-01 06:05:54 -0400

    wrap fn

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,3 +1,5 @@
+import { select as d3Select } from 'd3-selection'
+
 const fetcher = async (url, opts) => {
 	let fetchOpts = opts || {}
 	let fetchRes = await fetch(url, fetchOpts)
@@ -11,6 +13,31 @@ const fetcher = async (url, opts) => {
 	}
 }
 
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3Select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
+
 export {
-	fetcher
+	fetcher,
+	wrap
 }
