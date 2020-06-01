49317ccbd2510c58d57ec69e40385633b9884951
Eric Laursen <ericjlaursen@gmail.com>
2020-06-01 05:43:48 -0400

    init

---
+++ client/src/Components/Axis/Axis.js
@@ -0,0 +1,47 @@
+import React, { 
+	useState, 
+	useEffect,
+	useRef 
+} from 'react'
+import * as d3Axis from 'd3-axis'
+import { select as d3Select } from 'd3-selection'
+import './Axis.css'
+import { wrap } from '../../helpers'
+
+const Axis = (props) => {
+  
+  const axisElement = useRef()
+
+  //after loading <g> ref
+  useEffect(() => {
+  	if(axisElement && axisElement.current){
+  		let axisRef = axisElement.current
+  		const axisType = `axis${props.orient}`
+	    console.log('axisType')
+	    console.log(axisType)
+	    
+	  
+	    const axis = d3Axis[axisType]()
+	      .scale(props.scale)
+	      // .tickSize(-props.tickSize)
+	      // .tickPadding([12])
+	      // .ticks(5)
+
+	    d3Select(axisRef).call(axis)
+	    if(props.orient == 'bottom'){
+	      d3Select(axisRef).selectAll(".tick text")
+	      .call(wrap, props.scale.bandwidth())
+	    }
+  	}
+  },[axisElement])
+
+  return (
+    <g
+      className={`Axis Axis-${props.orient}`}
+      ref={axisElement}
+      // transform={props.translate}
+    />
+  )
+}
+
+export default Axis;
