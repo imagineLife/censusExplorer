87ebe5c27ab6474850049422acbea601d9c4d97d
Eric Laursen <ericjlaursen@gmail.com>
2020-06-02 07:28:41 -0400

    init, clone of BoxPlot

--- client/src/Components/BarChart/BarChart.js
+++ client/src/Components/BarChart/BarChart.js
@@ -1,5 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useDimensions from './../../Hooks/UseDimensions'
+import{ scaleLinear } from 'd3-scale'
 
-const BarChart = () => (<p>BarChart</p>);
+// Components
+import Axis from './../Axis'
+const BarChart = ({
+	min,
+	max,
+	median,
+	q1,
+	q3,
+	orientation,
+	axis,
+	h,
+	col
+}) => {
+	
+	//get dims, configured from props
+	const [ref, {width, height}] = useDimensions();
+	const inheritedPadding = 8;
+	const chartPadding = 16;
+	const whiteStroke = '#a9b7c9'
+	const boxFill = 'rgb(38,49,20)'
+
+
+	/*
+		SVG && Chart 'inner' Dimensions
+	*/
+	let twoPads;
+	let wLP, hLP;
+	let chartWidth, chartHeight;
+
+	if(width && height){
+		//padding
+		twoPads = inheritedPadding * 2
+		let chartDoublePad = chartPadding * 2;
+		
+		//svg dims without padding
+		wLP = width - twoPads
+		hLP = height - twoPads
+		
+		//chart dims with MORE padding
+		chartWidth = width - chartDoublePad;
+		chartHeight = height - chartDoublePad;
+
+	}
+
+	/*
+		SVG Props
+	*/
+	let svgProps = {}
+	if(width && height){
+		svgProps = {
+			className:"chart-svg",
+			width: width,
+			height: height,
+			transform: `translate(-${inheritedPadding}, -${inheritedPadding})`
+		}
+	}
+
+	/*
+		Figure Props
+	*/ 
+	const figProps = {
+		className:`boxplot widget ${col}`,
+		style: {height: h},
+		ref
+	}
+
+	/*
+		Scales
+	*/	
+	let xScale = null
+	let yScale = null
+	if(width && height){
+		//scales DO NOT account for transf/trans of elements
+		xScale = scaleLinear().domain([min,max]).range([chartPadding, chartWidth ])
+		yScale = scaleLinear().domain([0, 100]).range([chartHeight, chartPadding])
+	}
+
+	/* 
+		axis 
+	*/
+	let thisAxis = null
+	if(orientation && orientation === 'horizontal'){
+		thisAxis = <Axis 
+			orient={"Bottom"}
+			scale={xScale}
+			translate={`translate(0,${height - (chartPadding * 2 )})`}
+			width={width}
+		/>
+	}
+
+	return(
+		<figure {...figProps}>
+				{/* wait till useDimension finishes */}
+				{width && height && 
+					<svg {...svgProps}>
+						<g transform={`translate(${inheritedPadding},${inheritedPadding})`}>
+							<text 
+								className="plain-text"
+								alignmentBaseline="hanging">
+								Binned Quartiles
+							</text>
+
+							{axis && thisAxis}
+
+						</g>
+					</svg>
+				}
+		</figure>
+	)
+};
 
 export default BarChart;
