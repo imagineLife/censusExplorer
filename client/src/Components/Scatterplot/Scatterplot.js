236bbf6b0e76f54ab2bbe20f43d83d8f2fb32a2c
Eric Laursen <ericjlaursen@gmail.com>
2020-06-06 19:38:26 -0400

    init self-contained state

--- client/src/Components/Scatterplot/Scatterplot.js
+++ client/src/Components/Scatterplot/Scatterplot.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Scatterplot = () => (<p>Scatterplot</p>);
+const Scatterplot = () => {
+	const [yAxisKey, setYAxisKey] = useState('percentBelowPoverty.gender.female');
+	const [yAxisVals, setYAxisVals] = useState(null)
+
+	useEffect(() => {
+		if(!yAxisVals){
+			console.log('FETCH yAxisVals here...');
+			setYAxisVals([1,2,3])
+		}
+	},[yAxisVals])
+	return (<p>Scatterplot</p>)
+};
 
 export default Scatterplot;
