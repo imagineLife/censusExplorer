5e0c3ef22a3893da3ef13ab5b2079b6c3b26ec56
Eric Laursen <ericjlaursen@gmail.com>
2020-06-06 20:10:02 -0400

    import singleStatHandler

--- server/handlers/index.js
+++ server/handlers/index.js
@@ -2,9 +2,11 @@ const { shapeHandler } = require('./shape')
 const { statsKeysHandler } = require('./statsKeys')
 const { tableHandler } = require('./table')
 const { statisticHandler } = require('./statistic')
+const { singleStatHandler } = require('./singleStat')
 module.exports = {
 	shapeHandler,
 	tableHandler,
 	statisticHandler,
-	statsKeysHandler
+	statsKeysHandler,
+	singleStatHandler
 }
